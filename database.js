21337f3ab5bf0d646f21b626fce6eec43e56064a
Fatma Rümeysa Göllü <rumeysagollu@gmail.com@gmail.com>
2016-01-06 00:45:05 +0200

    katalog uygulaması

--- database.js
+++ database.js
@@ -6,12 +6,26 @@ var catalog = [
         "itemCreationDate":"2015",
         "itemDescription":"<p>Çalışma cehennemine fırlatılmış bedenlerin trajedisi işlenir Kafka'nın Dönüşüm'ünde. Hepimizin başına gelen bir trajedi bu aslında: Hayatta kalmak için çalışmak zorundayız. Çalışmanın 'zorunluluk' olduğu bir dünyada 'özgürlüğe' yer olmadığı açık. Dönüşüm'ün satır aralarında köleliğin zincir sesleri duyulur. Aile kurumuna sonuna kadar bağlı olan, baba otoritesinden mustarip, borç batağına saplanmış bir karakterin; kendini bir sabah bir böceğe dönüşmüş olarak bulan Gregor Samsa'nın hazin hikâyesi, modern bireyin hikâyesiyle çakışır. Kapitalist toplumsal düzeneğin alelade bir karakteri olan Gregor Samsa özelinde, her gün aynı saatte uyanmak zorunda olan; akrep ve yelkovanın akışına bağlı olarak hareket eden bedenlerin hayatta kalmak için nasıl çırpındıkları anlatılır.<br>(Tanıtım Bülteninden)</p>"
     }, 
+    {
+        "itemName":"Milena'ya Mektuplar",
+        "itemAuthor":"Franz Kafka",
+        "itemImage":"images/milenayamektuplar.jpg",
+        "itemCreationDate":"2013",
+        "itemDescription":"<p>Her şeye rağmen, mutluluktan ölünebiliyorsa, o zaman kesinlikle bu şekilde öleceğim. Ayrıca, ölüm döşeğindeki birisi, mutluluk sayesinde hayata tutunabiliyorsa o zaman ben de hayatta kalacağım.Aldığın çiçekler için çok üzgünüm, o kadar üzgünüm ki ne tür çiçekler olduğunu bile okuyamadım. Şimdi senin odanda duruyorlar. Eğer gerçekten odandaki gardırobun yerinde olsaydım, gündüz kendimi bir şekilde odanın dışına atar ve en azından çiçekler solana kadar salonda dururdum. Hayır, bu hiç de hoş değil. Ve o kadar uzakta ki her şey ama hâlâ odanın kapısının kolunu görebiliyorum, bana gözlerimin önündeki mürekkep hokkası kadar yakın.Seni seviyorum işte, budala, deniz dibindeki çakıl taşı nasıl sevilip, sarmalanır, ona bağlanılırsa ben de sana öyle bağlıyım.<br>(Tanıtım Bülteninden)</p>"
+    }, 
 	{
         "itemName":"Fareler ve İnsanlar",
         "itemAuthor":"John Steinbeck",
         "itemImage":"images/farelerveinsanlar.jpg",
         "itemCreationDate":"2012",
         "itemDescription":"<p>Pulitzer ve Nobel Edebiyat Ödülü'nü kazanan John Steinbeck'in çağımızın toplumsal ve insani meselelerini ustalıkla resmettiği eserleri modern dünya edebiyatının başyapıtları arasında yer alır. Steinbeck romanlarında yalın ve keskin bir gerçeklik sunarken yine de her seferinde çarpıcı bir öykü ile çıkar okurunun karşısına. Tarihin bir kesitindeki dramı insani ayrıntıları kaçırmadan sergilerken, tozpembe olmayan gerçekçi bir umudun türküsünü dillendirir. Bu nedenle eserleri edebi değerleri kadar güncelliklerini de hiç yitirmemiştir. Fareler ve İnsanlar, birbirine zıt karakterdeki iki mevsimlik tarım işçisinin, zeki George Milton ve onun güçlü kuvvetli ama akli dengesi bozuk yoldaşı Lennie Small'un öyküsünü anlatır. Küçük bir toprak satın alıp insanca bir hayat yaşamanın hayalini kuran bu ikilinin öyküsünde dostluk ve dayanışma duygusu önemli bir yer tutar. Steinbeck insanın insanla ilişkisini anlatmakla kalmaz insanın doğayla ve toplumla kurduğu ilişkileri de konu eder bu destansı romanında. Kitabın ismine ilham veren Robert Burns şiirindeki gibi;En iyi planları farelerin ve insanların / Sıkça ters gider...<br>(Tanıtım Bülteninden)</p>"
+    }, 
+    {
+        "itemName":"İnci",
+        "itemAuthor":"John Steinbeck",
+        "itemImage":"images/inci.jpg",
+        "itemCreationDate":"2012",
+        "itemDescription":"<p>Pulitzer ve Nobel Edebiyat Ödülü'ne layık görülen John Steinbeck'in çağımızın toplumsal ve insani meselelerini ustalıkla resmettiği eserleri modern dünya edebiyatının başyapıtları arasında yer alır. Tomris Uyar'ın sunuş yazısında belirttiği gibi, İnsanoğlunun umudunun, var olma direncinin seyreldiği bir tarih anında olanca görkemiyle gerçek umudun türküsünü söylemiştir. Tozpembe olmayan gerçekçi bir umudun. Bu nedenle eserleri edebi değerleri kadar güncelliklerini de hiç yitirmemiştir. Bir Meksika halk hikâyesinden esinlenmiş İnci, bir zamanlar İspanya Kralı'na büyük zenginlikler getiren bir koyda yaşayan fakir bir inci avcısının, Kino'nun ve ailesinin hikâyesini anlatır. Kino'nun çocuğunu kurtarmak umuduyla daldığı denizden çıkardığı eşi benzeri görülmemiş inci, yalnızca umut değil yıkım da getirecektir. İncinin özü insanların özüne; Kino'nun kulaklarında çınlayan ve kasabaya yayılan İncinin Türküsü, ailenin, kötülüğün, umudun ve düşmanlığın türküsüne karışacaktır. Steinbeck, Kino'nun derinliklerden söküp çıkardığı inci ile içinde yaşadığımız dünyaya ve insanın dramına ışık tutuyor.<br>(Tanıtım Bülteninden)</p>"
     }, 
 	{
         "itemName":"Çocukluğum",
@@ -21,11 +35,26 @@ var catalog = [
         "itemDescription":"<p>Gorki’nin kendi hayatından etkilenerek yazdığı, birbirinin devamı olan üç eser vardır: Çocukluğum, Ekmeğimi Kazanırken ve Benim Üniversitelerim. Dünya klasikleri arasında yeri doldurulamayacak eserler olan bu kitaplar milyonlarca satarak, severek okunmuştur. Gorki’nin çocukluğu yoksulluk içinde, değişik kesimlerden, cahil, kaba insanlar arasında geçmiştir. Bu insanlar eserlerinde o kadar iyi anlatılmışlar ve incelenmişlerdir ki, bu görüşün altında sımsıcak insan sevgisi, işlenmemiş bir duyarlılık vardır. Aç, kültürsüz, bilgiye susayan bu insanlardan yeni bir insan, yeni bir toplum ortaya çıkacaktır..<br>(Tanıtım Bülteninden)</p>"
     },
     {
+        "itemName":"Benim Üniversitelerim",
+        "itemAuthor":"Maksim Gorki",
+        "itemImage":"images/benimüniversitelerim.jpg",
+        "itemCreationDate":"2015",
+        "itemDescription":"<p>Benim Üniversitelerim, Maksim Gorki´nin hayatını yazdığı üç kitaptan sonuncusudur. İlk başta okuma azmiyle üniversiteye başladı gibi görünse de üniversite için gittiği şehirde okuyamamış, öğrencilerle beraber kalarak çalışma hayatına devam etmiştir. Devrimci kişilireler beraber yaşamaya başlamıştır. Siyasi düşüncelerinin geliştiği de önemli ölçüde hissedilir ki üniversitelerim olarak yaşadığı tecrübeleri belitmek istemiştir. Bir fırıncıda çalışırken intihar eder ve hastaneden çıktığında da bir Ukraynalı´nın yanına gider. Böyece çar rejimine karşı daha da direnişli olur. Okumayı da bırakmaz. Başına gelen bir dizi felaket olur ve kitaplardan okuduğu köylülerin aslında nasıl olduğunu betimler.<br>(Tanıtım Bülteninden)</p>"
+    },
+    
+    {
         "itemName":"Ölü Canlar",
         "itemAuthor":"Nikolay Vasilyeviç Gogol",
         "itemImage":"images/olu_canlar.jpg",
         "itemCreationDate":"2015",
         "itemDescription":"<p>Gogol, 19. yüzyıla damgasını vuran Ölü Canlar romanını başlangıçta üç cilt olarak tasarlamış, ikinci cildini ölmeden önce yok etmiş, üçüncü cildini yazmamıştır. Buna rağmen 1842'de yayınlanan Ölü Canlar bütünlüklü ve Rusya'daki yaşamı en ince ayrıntısına kadar yansıtan bir kitaptır. Gogol bu kitabıyla ülkesindeki çarpıklıkları sergilemiştir. <br>(Tanıtım Bülteninden)</p>"
+    },
+     {
+        "itemName":"Palto",
+        "itemAuthor":"Nikolay Vasilyeviç Gogol",
+        "itemImage":"images/palto.jpg",
+        "itemCreationDate":"2015",
+        "itemDescription":"<p>Toplumsal hayatın kıyısında yer alan silik bir karakterin, Akakiy Akakiyeviç adlı sıradan bir devlet memurunun trajikomik hikâyesi anlatılıyor Gogol'ün Palto'sunda. Trajik olanla komik olanın ustaca bir araya getirildiği bu hikâye, gerek Rus edebiyatını gerekse dünya edebiyatını yarattığı karakter itibariyle derinden etkilemiştir. Modern hayatın bürokratik mekanizmaları içine çekilmiş, para sıkıntısı çeken, işini kaybetmekten ürken, çevresindeki insanlar tarafından sürekli aşağılanan, ezilip hor görülen bir memurdur Akakiyeviç. Kendini gerçekleştirdiği tek eylemse, şevkle yaptığı işidir: Bu işte, yani mektupları temize çekme işinde sadece kendine ait, değişik, hoş bir dünya bulurdu. İşini yaparken duyduğu mutluluk yüzünden okunurdu; bazı harfler gözdesiydi ve sıra bu harfleri yazmaya geldiğinde kendinden geçer, gözlerini kırpıştırır, gülümser ve sanki dudaklarıyla kalemine yardım ederdi. Yüzüne bakınca kaleminin ucundan hangi harfin dökülmekte olduğu kolayca anlaşılırdı.Kara kışın soğuğundan korunmak için bir paltoya ihtiyaç duyan Akakiyeviç'in, bu paltoyu güç bela edinmesiyle birlikte bütün hayatı altüst olur, komik olan yerini trajik olana bırakır…  <br>(Tanıtım Bülteninden)</p>"
     },
     {
         "itemName":"Babalar ve Oğulları",
@@ -33,6 +62,13 @@ var catalog = [
         "itemImage":"images/babalar_ve_ogulları.jpg",
         "itemCreationDate":"2005",
         "itemDescription":"<p>Rus edebiyatının en önemli isimlerinden hikayeci, oyun ve deneme yazarı, şair Turgenyev, varlıklı bir ailenin çocuğuydu. Moskova ve Petersburg'da felsefe, tarih ve edebiyat eğitimi aldı. Daha sonra, Berlin Üniversitesi'nde Schelling'in derslerine katıldı, Hegel incelemelerinde yer aldı. 1840'larda yazarlığa başlayan Turgenyev, yapıtlarında ince duygulu karakter çizimlerine, müziksel bir anlatıma yer verir. Turgenyev'in de başyapıtı olan 'Babalar ve Oğullar' dünya edebiyatının en önemli klasik eserlerindendir. Turgenyev, 1800'lerin sonunda geçen bu romanında, kahramanı Bazarov'u merkez olarak o dönemin Rusya'sındaki kuşatlar arası çatışmayı ustalıkla anlatırken; eski ile yeniyi ve yenilikçilik ile tutuculuğu karşılaştırmıştır.<br>(Tanıtım Bülteninden)</p>"
+    },
+    {
+        "itemName":"Babalar ve Oğullar",
+        "itemAuthor":"Ivan Sergeyeviç Turgenyev",
+        "itemImage":"images/babalar.jpg",
+        "itemCreationDate":"2015",
+        "itemDescription":"<p>'Romanda, geçen yüzyıl Rusya'sının toplumsal-siyasal görünümü ele alınmaktadır. Romandaki baba ve oğul karakterleri iki Rus jenerasyonu arasındaki artan bölünmüşlüğü, Yevgeniy Bazarov ise nihilist görüşleri ve eski düzen karşıtlığı ile ilk Bolşevikleri temsil eder.Dönemin Rusya'sında yaşanan geleneksellik ile bireysellik arasındaki çatışmanın adım adım gösterildiği romanda, adından da anlaşılacağı gibi babalar kuşağı, ataerkil toplumun sarsılmaz töresel inancını; oğullar ise, bütün töreleri yok sayma savaşını temsil ederler.Babalar ve Oğullar, Rus Edebiyatı'nın tam anlamıyla yazılmış ilk modern roman örneği olarak kabul edilebilir. Aynı zamanda batı dünyasında şöhret kazanan ilk Rus edebiyat çalışmasıdır.<br>(Tanıtım Bülteninden)</p>"
     },
     {
         "itemName":"İlk Aşk",
@@ -47,5 +83,13 @@ var catalog = [
         "itemImage":"images/bilgelik_takvimi.jpg",
         "itemCreationDate":"2003",
         "itemDescription":"<p>O adam. Uzunca sakallı. Elinde kalem; yazan adam. O yaşlı adam. Zamanın en şaşaalı, en gürültülü dönüşümlerinin yaşandığı bir yüzyılda sadece içinin fırtınalarına kulak kabartmış bir münzevi. Koyu zulümlere, katı ayırımlara zemin olmuş bir mekanın ortasından bulanmadan kalbimize akmış bir büyük düşünce ırmağı. Tolstoy, çok görmüş ve çok yaşamış 'bir suskun bilge dede' imajını tamamlar çoğu okurunun dünyasında. Sanki anlatmak için torunlarının sormasını bekliyor gibidir. Onun uzun sakallarına tutunan zamanlar ne kadar eski ve koyu ise, kaleminden beyaz sayfalara düşen notlar bir o kadar yeni ve tazedir. Zihinlerimizde yer eden o yaşlı görüntüsünün aksine, Tolstoy, milliyetin ve mekanın üzerine çıkabilmiş, zamanın ve alışkanlıkların yukarısında durabilmiş, her an değişime hazır, her an dönüşmeye çağıran bir genç sesin temsilcisi olarak dolaşıyor aramızda şimdi. Tolstoy'u hem bir bilge dede hem heyecanlı bir genç ses olarak görmenin çelişkisi, Her Güne Beş Düşünce'de çözülür. Tolstoy, bu özel eserinde, tıpkı yeni yetme bir delikanlı gibi kendi düşüncelerini besleyecek ve hayallerini zenginleştirecek sözleri toparlıyor. Kendi çağdaşları da içinde olmak üzere bir çok düşünürden süzerek topladığı sözleri, yılın her gününe yayıyor, yer yer sözlere suskun bilge dedenin üslubunu da katıyor. Her Güne Beş Düşünce, Tolstoy'u sadece bir yazar olarak değil, bir editör olarak, bir seçkiler çalışan bir yayıncı olarak karşımıza çıkarır. Kendisini aydınlatan sözleri, kendi aydınlığı içinde bizlere sunarken, okumayı az da olsa her günün alışkanlığı haline getirmemiz de telkin ediyor.<br>(Tanıtım Bülteninden)</p>"
+    },
+     {
+        "itemName":"İnsan Ne İle Yaşar",
+        "itemAuthor":"Lev Nikolayeviç Tolstoy",
+        "itemImage":"images/insanneileyasar.jpg",
+        "itemCreationDate":"2015",
+        "itemDescription":"<p>Tolstoy'un,halk için yapılan bir edebiyat nasıl olmalı tartışmalarına yanıt olarak kaleme aldığı hikâyeler bütün dünyada sürekli bir esin kaynağı olmuştur. Konularını daha çok Kutsal Kitap metinlerinden ve halk hikâyelerinden alan Tolstoy, bu hikâyelerinde insanlığı doğruluğa, dürüstlüğe, sevgiye davet eder. İnsan ne ile yaşar? unutulmaz hikâyeler arasına girerken, Bir insana ne kadar toprak lazım? adlı hikâye James Joyce tarafından övülmüş, Wittgenstein'ın sevdiği hikâyeler arasında yer almıştır. Bu hikâyelerin kahramanları genellikle soylular değil, yoksul insanlardır ve Mum adlı hikâyede olduğu gibi halkın angarya ve zorbalığa karşı öfkesi çok açık ifadelerle verilir. Rusça aslından çevrilen bu derleme, günümüzde de Tolstoy'un düşüncesinin ve hayatının merkezi olarak ziyaret edilen Yasnaya Polyana Müzesi yöneticilerinden Galina Alekseyeva'nın önsözüyle sunuluyor.<br>(Tanıtım Bülteninden)</p>"
     }
+    
 ];
