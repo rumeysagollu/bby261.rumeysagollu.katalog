5ce4d015375e6c1ff18475d0eb3a7e24eb90374c
Fatma Rümeysa Göllü <rumeysagollu@gmail.com@gmail.com>
2016-01-05 22:32:51 +0200

    katalog uygulaması

---
+++ database.js
@@ -0,0 +1,51 @@
+var catalog = [
+	{
+        "itemName":"Dönüşüm",
+        "itemAuthor":"Franz Kafka",
+        "itemImage":"images/donusum.jpg",
+        "itemCreationDate":"2015",
+        "itemDescription":"<p>Çalışma cehennemine fırlatılmış bedenlerin trajedisi işlenir Kafka'nın Dönüşüm'ünde. Hepimizin başına gelen bir trajedi bu aslında: Hayatta kalmak için çalışmak zorundayız. Çalışmanın 'zorunluluk' olduğu bir dünyada 'özgürlüğe' yer olmadığı açık. Dönüşüm'ün satır aralarında köleliğin zincir sesleri duyulur. Aile kurumuna sonuna kadar bağlı olan, baba otoritesinden mustarip, borç batağına saplanmış bir karakterin; kendini bir sabah bir böceğe dönüşmüş olarak bulan Gregor Samsa'nın hazin hikâyesi, modern bireyin hikâyesiyle çakışır. Kapitalist toplumsal düzeneğin alelade bir karakteri olan Gregor Samsa özelinde, her gün aynı saatte uyanmak zorunda olan; akrep ve yelkovanın akışına bağlı olarak hareket eden bedenlerin hayatta kalmak için nasıl çırpındıkları anlatılır.<br>(Tanıtım Bülteninden)</p>"
+    }, 
+	{
+        "itemName":"Fareler ve İnsanlar",
+        "itemAuthor":"John Steinbeck",
+        "itemImage":"images/farelerveinsanlar.jpg",
+        "itemCreationDate":"2012",
+        "itemDescription":"<p>Pulitzer ve Nobel Edebiyat Ödülü'nü kazanan John Steinbeck'in çağımızın toplumsal ve insani meselelerini ustalıkla resmettiği eserleri modern dünya edebiyatının başyapıtları arasında yer alır. Steinbeck romanlarında yalın ve keskin bir gerçeklik sunarken yine de her seferinde çarpıcı bir öykü ile çıkar okurunun karşısına. Tarihin bir kesitindeki dramı insani ayrıntıları kaçırmadan sergilerken, tozpembe olmayan gerçekçi bir umudun türküsünü dillendirir. Bu nedenle eserleri edebi değerleri kadar güncelliklerini de hiç yitirmemiştir. Fareler ve İnsanlar, birbirine zıt karakterdeki iki mevsimlik tarım işçisinin, zeki George Milton ve onun güçlü kuvvetli ama akli dengesi bozuk yoldaşı Lennie Small'un öyküsünü anlatır. Küçük bir toprak satın alıp insanca bir hayat yaşamanın hayalini kuran bu ikilinin öyküsünde dostluk ve dayanışma duygusu önemli bir yer tutar. Steinbeck insanın insanla ilişkisini anlatmakla kalmaz insanın doğayla ve toplumla kurduğu ilişkileri de konu eder bu destansı romanında. Kitabın ismine ilham veren Robert Burns şiirindeki gibi;En iyi planları farelerin ve insanların / Sıkça ters gider...<br>(Tanıtım Bülteninden)</p>"
+    }, 
+	{
+        "itemName":"Çocukluğum",
+        "itemAuthor":"Maksim Gorki",
+        "itemImage":"images/cocuklugum.jpg",
+        "itemCreationDate":"2008",
+        "itemDescription":"<p>Gorki’nin kendi hayatından etkilenerek yazdığı, birbirinin devamı olan üç eser vardır: Çocukluğum, Ekmeğimi Kazanırken ve Benim Üniversitelerim. Dünya klasikleri arasında yeri doldurulamayacak eserler olan bu kitaplar milyonlarca satarak, severek okunmuştur. Gorki’nin çocukluğu yoksulluk içinde, değişik kesimlerden, cahil, kaba insanlar arasında geçmiştir. Bu insanlar eserlerinde o kadar iyi anlatılmışlar ve incelenmişlerdir ki, bu görüşün altında sımsıcak insan sevgisi, işlenmemiş bir duyarlılık vardır. Aç, kültürsüz, bilgiye susayan bu insanlardan yeni bir insan, yeni bir toplum ortaya çıkacaktır..<br>(Tanıtım Bülteninden)</p>"
+    },
+    {
+        "itemName":"Ölü Canlar",
+        "itemAuthor":"Nikolay Vasilyeviç Gogol",
+        "itemImage":"images/olu_canlar.jpg",
+        "itemCreationDate":"2015",
+        "itemDescription":"<p>Gogol, 19. yüzyıla damgasını vuran Ölü Canlar romanını başlangıçta üç cilt olarak tasarlamış, ikinci cildini ölmeden önce yok etmiş, üçüncü cildini yazmamıştır. Buna rağmen 1842'de yayınlanan Ölü Canlar bütünlüklü ve Rusya'daki yaşamı en ince ayrıntısına kadar yansıtan bir kitaptır. Gogol bu kitabıyla ülkesindeki çarpıklıkları sergilemiştir. <br>(Tanıtım Bülteninden)</p>"
+    },
+    {
+        "itemName":"Babalar ve Oğulları",
+        "itemAuthor":"Ivan Sergeyeviç Turgenyev",
+        "itemImage":"images/babalar_ve_ogulları.jpg",
+        "itemCreationDate":"2005",
+        "itemDescription":"<p>Rus edebiyatının en önemli isimlerinden hikayeci, oyun ve deneme yazarı, şair Turgenyev, varlıklı bir ailenin çocuğuydu. Moskova ve Petersburg'da felsefe, tarih ve edebiyat eğitimi aldı. Daha sonra, Berlin Üniversitesi'nde Schelling'in derslerine katıldı, Hegel incelemelerinde yer aldı. 1840'larda yazarlığa başlayan Turgenyev, yapıtlarında ince duygulu karakter çizimlerine, müziksel bir anlatıma yer verir. Turgenyev'in de başyapıtı olan 'Babalar ve Oğullar' dünya edebiyatının en önemli klasik eserlerindendir. Turgenyev, 1800'lerin sonunda geçen bu romanında, kahramanı Bazarov'u merkez olarak o dönemin Rusya'sındaki kuşatlar arası çatışmayı ustalıkla anlatırken; eski ile yeniyi ve yenilikçilik ile tutuculuğu karşılaştırmıştır.<br>(Tanıtım Bülteninden)</p>"
+    },
+    {
+        "itemName":"İlk Aşk",
+        "itemAuthor":"Ivan Sergeyeviç Turgenyev",
+        "itemImage":"images/ilk_ask.jpg",
+        "itemCreationDate":"2008",
+        "itemDescription":"<p>'İlk Aşk'ın yazarı Turgenyev, sadece Rusya'nın değil, Dünyanın ölümsüz yazarları arasında yer almaktadır. Bu romanının kahramanı Vladimir'de kendisinden başkası değildir. Ailesi ile on altı yaşlarında yaşamış olduğu duyguları içtenlikle anlatmak için 42 yaşlarında 'İLK AŞK'ı kaleme almıştır. Geride bıraktığı o yıllardaki hislerini şöyle dillendirir: 'Ey, kısa ömürlü fırtınalar, gönülden gönüle çağlayan sesler, tertemiz bağlantılar, duygulu, düş dolu ruhlar... İlk SEvginin, ilk aşkın benliği saran sevinci! Siz nerdesiniz, nerdesiniz? Turgenyev'in yaşadığı 'İlk Aşk2ta, rakip olarak karşısına maalesef beklenmedik bir kader çizgisiyle öz babası çıkmıştır. Ne var ki, babası oğlunun yoğrulmakta olduğu o duygularından habersizdir... Yazarın yenilgiyle son bulan ve karmaşık hislerle yüklü o anıları, daha sonraki yaşamın da onu derinden etkilemiştir. Bu eserle birlikte, KARAMZİN'in 'Tüm Rusların ta okul sıralarında öğrendiği, yaşanmış, gerçek hayattan alınmış 'TALİHSİZ LİZA' adlı hikâyesine de yer verdik. Bu hikâye, tüm Rus halkının üzerinde öylesine tesir bırakmış ki, hatırası halen bugün dahi canlı tutulmaktadır. Karamzin, Liza'nın saf aşkını ve hazin sonunu, okuyucuları hemen etkileyecek bir biçimde, duygu dolu, nefis doğa tasvirleri ile bezenmiş bir anlatımla işlemiştir. 'İlk Aşk' ve 'Talihsiz Liza', diplomat-yazar Alhan Altan ARASLI tarafından Rusça aslından çevrilmiştir. Kitapta, ayrıca, Araslı'nın, Turgenyev ve Karamzin'in eserleri üzerine yaptığı bir incelemeye de yer verilmiştir.<br>(Tanıtım Bülteninden)</p>"
+    },
+    {
+        "itemName":"Bilgelik Takvimi",
+        "itemAuthor":"Lev Nikolayeviç Tolstoy",
+        "itemImage":"images/bilgelik_takvimi.jpg",
+        "itemCreationDate":"2003",
+        "itemDescription":"<p>O adam. Uzunca sakallı. Elinde kalem; yazan adam. O yaşlı adam. Zamanın en şaşaalı, en gürültülü dönüşümlerinin yaşandığı bir yüzyılda sadece içinin fırtınalarına kulak kabartmış bir münzevi. Koyu zulümlere, katı ayırımlara zemin olmuş bir mekanın ortasından bulanmadan kalbimize akmış bir büyük düşünce ırmağı. Tolstoy, çok görmüş ve çok yaşamış 'bir suskun bilge dede' imajını tamamlar çoğu okurunun dünyasında. Sanki anlatmak için torunlarının sormasını bekliyor gibidir. Onun uzun sakallarına tutunan zamanlar ne kadar eski ve koyu ise, kaleminden beyaz sayfalara düşen notlar bir o kadar yeni ve tazedir. Zihinlerimizde yer eden o yaşlı görüntüsünün aksine, Tolstoy, milliyetin ve mekanın üzerine çıkabilmiş, zamanın ve alışkanlıkların yukarısında durabilmiş, her an değişime hazır, her an dönüşmeye çağıran bir genç sesin temsilcisi olarak dolaşıyor aramızda şimdi. Tolstoy'u hem bir bilge dede hem heyecanlı bir genç ses olarak görmenin çelişkisi, Her Güne Beş Düşünce'de çözülür. Tolstoy, bu özel eserinde, tıpkı yeni yetme bir delikanlı gibi kendi düşüncelerini besleyecek ve hayallerini zenginleştirecek sözleri toparlıyor. Kendi çağdaşları da içinde olmak üzere bir çok düşünürden süzerek topladığı sözleri, yılın her gününe yayıyor, yer yer sözlere suskun bilge dedenin üslubunu da katıyor. Her Güne Beş Düşünce, Tolstoy'u sadece bir yazar olarak değil, bir editör olarak, bir seçkiler çalışan bir yayıncı olarak karşımıza çıkarır. Kendisini aydınlatan sözleri, kendi aydınlığı içinde bizlere sunarken, okumayı az da olsa her günün alışkanlığı haline getirmemiz de telkin ediyor.<br>(Tanıtım Bülteninden)</p>"
+    }
+];
